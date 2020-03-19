b2a14be30c9326d58757cca89da5c9e81f5eefe3
JoelTi <61313145+JoelTi@users.noreply.github.com>
2020-03-19 11:48:14 +0100

    map api

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function showMap() {
+  var map = tt.map({
+    key: 'YGOSC3vaMIRj32nNnQoYktX5D3svhpM2',
+    container: 'map',
+    style: 'tomtom://vector/1/basic-main',
+    center: [8, 51],
+    zoom: 4
+  });
+
+  var config = {
+    key: 'YGOSC3vaMIRj32nNnQoYktX5D3svhpM2',
+    style: 'tomtom://vector/1/relative',
+    refresh: 30000
+  };
+
+  map.on('load', function() {
+    map.addTier(new tt.TrafficFlowTilesTier(config));
+  });
+}
+
+window.onload = function() {
+  showMap();
+}
