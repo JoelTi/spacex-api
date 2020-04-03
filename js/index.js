a64c11d4071c0f31803410ae6bdacae177bb188b
JoelTi <61313145+JoelTi@users.noreply.github.com>
2020-04-03 15:35:14 +0200

    mapbox

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-function showMap() {
-  var map = tt.map({
-    key: '2W0tot7r2PRIPMIkrBmPYXDPrC24AfKj',
-    container: 'map',
-    style: 'tomtom://vector/1/basic-main',
-    center: [8, 51],
-    zoom: 4
-  });
-
-  var config = {
-    key: '2W0tot7r2PRIPMIkrBmPYXDPrC24AfKj',
-    style: 'tomtom://vector/1/relative'
-  };
-
-  map.on('load', function() {
-    map.addTier(new tt.TrafficFlowTilesTier(config));
-  });
-  map.addControl(new tt.GeolocateControl({
-   positionOptions: {
-     enableHighAccuracy: true
-   },
-    trackUserLocation: true
-  }));
-
-}
+// function showMap() {
+//   var map = tt.map({
+//     key: '2W0tot7r2PRIPMIkrBmPYXDPrC24AfKj',
+//     container: 'map',
+//     style: 'tomtom://vector/1/basic-main',
+//     center: [8, 51],
+//     zoom: 4
+//   });
+//
+//   var config = {
+//     key: '2W0tot7r2PRIPMIkrBmPYXDPrC24AfKj',
+//     style: 'tomtom://vector/1/relative'
+//   };
+//
+//   map.on('load', function() {
+//     map.addTier(new tt.TrafficFlowTilesTier(config));
+//   });
+//   map.addControl(new tt.GeolocateControl({
+//    positionOptions: {
+//      enableHighAccuracy: true
+//    },
+//     trackUserLocation: true
+//   }));
+//
+// }
 
 function setTimer() {
   var time = document.getElementsByTagName('time')[0],
@@ -229,9 +229,141 @@ function onAPIError(error) {
   document.getElementById('btn-weergeven').classList.add('btn-animation');
 }
 
+function showMapBox() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiMTYwNzE5MjEiLCJhIjoiY2s4azE5dTRlMDEzOTNubms3eGFrNG5oZiJ9.1Ay5wHAcfjYWywQ3b2m51g';
+
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+				center: [50, 50],
+				zoom: 1.5,
+    });
+
+    var size = 200;
+
+
+    // implementation of CustomLayerInterface to draw a pulsing dot icon on the map
+    // see https://docs.mapbox.com/mapbox-gl-js/api/#customlayerinterface for more info
+    var pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+        // get rendering context for the map canvas when layer is added to the map
+        onAdd: function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
+
+        // called once before every frame where the icon will be used
+        render: function() {
+            var duration = 1000;
+            var t = (performance.now() % duration) / duration;
+
+            var radius = (size / 3) * 0.3;
+            var outerRadius = (size / 2) * 0.7 * t + radius;
+            var context = this.context;
+
+            // draw outer circle
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                outerRadius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+            context.fill();
+
+            // draw inner circle
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                radius,
+                0,
+                Math.PI * 2
+            );
+            context.fillStyle = 'rgba(255, 100, 100, 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * (1 - t);
+            context.fill();
+            context.stroke();
+
+            // update this image's data with data from the canvas
+            this.data = context.getImageData(
+                0,
+                0,
+                this.width,
+                this.height
+            ).data;
+
+            // continuously repaint the map, resulting in the smooth animation of the dot
+            map.triggerRepaint();
+
+            // return `true` to let the map know that the image was updated
+            return true;
+        }
+    };
+
+    map.on('load', function() {
+        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+        map.addSource('points', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                    {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [70, 50]
+                        }
+                    }
+                ]
+            }
+        });
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'points',
+            'layout': {
+                'icon-image': 'pulsing-dot'
+            }
+        });
+				map.addControl(
+					new MapboxGeocoder({
+					accessToken: mapboxgl.accessToken,
+					mapboxgl: mapboxgl
+					})
+				);
+				map.addControl(
+					new mapboxgl.NavigationControl()
+				);
+				map.addControl(
+					new mapboxgl.GeolocateControl({
+						positionOptions: {
+						enableHighAccuracy: true
+						},
+						trackUserLocation: true
+					})
+				);
+				map.addControl(
+					new mapboxgl.FullscreenControl()
+				);
+
+
+    });
+}
 
 window.onload = function() {
   myChart();
-  showMap();
+  // showMap();
+  showMapBox();
   setTimer();
 }
